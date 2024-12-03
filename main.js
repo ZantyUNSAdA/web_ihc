239d3f8e2ea019bf509aacb8c8ac02f9d54db2bd
ZantyUNSAdA <szeballos@alumnos.unsada.edu.ar>
2024-12-03 13:58:59 -0300

    scroll button y altura del boton

---
+++ main.js
@@ -0,0 +1,85 @@
+
+// Calculadora IMC.
+function calculate(){
+    var height= (document.getElementById("height").value)/100;
+    var weight = document.getElementById("weight").value;
+
+    var imc = weight / height ** 2;
+    var resultado=""
+    if(imc<18.5){
+        resultado="Usted se encuentra por debajo del peso normal.";
+    }else if(imc<24.9){
+        resultado="Usted se encuentra en un peso normal!";
+    }else if(imc<39.9){
+        resultado="Usted se encuentra por encima del peso normal.";
+    }else if(imc>39.9){
+        resultado="Usted se encuentra con obesidad.";
+    }
+    document.getElementById("text_area").innerText=resultado;
+}
+
+// Verificar campos llenos en el formulario.
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+
+      form.classList.add('was-validated');
+
+      // Custom validation for showing/hiding invalid feedback messages
+      const invalidFeedbackElements = form.querySelectorAll('.invalid-feedback');
+      let isValid = true;
+      for (let i = 0; i < form.elements.length - 1; i++) { // Exclude the submit button
+        const input = form.elements[i];
+        if (input.checkValidity() === false) {
+          isValid = false;
+          invalidFeedbackElements[i].style.display = 'block';
+        } else {
+          invalidFeedbackElements[i].style.display = 'none';
+        }
+      }
+    }, false);
+  });
+
+  
+    // Agrega un evento 'click' al botón de envío (btn_Enviar)
+    const btn_Enviar = document.getElementById('btn_Enviar'); // Asegúrate de obtener el botón correctamente
+  
+    btn_Enviar.addEventListener('click', function() {
+      // Verificar si el formulario es válido antes de mostrar SweetAlert
+      if (document.querySelector('.needs-validation').checkValidity()) {
+        mostrarSweetAlert();
+      }
+    });
+  
+    // Define la función mostrarSweetAlert para mostrar la ventana de SweetAlert
+    function mostrarSweetAlert() {
+      Swal.fire({
+        title: '¡Solicitud exitosa!',
+        text: '¡Muchas gracias por elegirnos! Nos pondremos en contacto contigo en las próximas 48 horas.',
+        icon: 'success',
+        confirmButtonText: 'Cerrar',
+        background: '#e1f4e8',
+        //footer: 'Texto en el pie de la alerta' ,
+      });
+    }
+  })();
+
+  // scrollTop Button
+const scrollTopButton = document.getElementById('scrollTopButton');
+
+scrollTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
